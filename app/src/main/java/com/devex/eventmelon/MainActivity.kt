60a89e3eec84f785b11e640acc898215c82678f4
Amal Joe R S <amaljoe88@gmail.com>
2021-02-13 21:18:45 +0530

    add fragments

--- app/src/main/java/com/devex/eventmelon/MainActivity.kt
+++ app/src/main/java/com/devex/eventmelon/MainActivity.kt
@@ -2,10 +2,33 @@ package com.devex.eventmelon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.devex.eventmelon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.bottom_nav_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val topLevelDestinations = setOf(R.id.homeFragment, R.id.exploreFragment, R.id.socialFragment, R.id.profileFragment)
+        val appBarConfiguration = AppBarConfiguration.Builder(topLevelDestinations).build()
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        binding.bottomNavBar.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id in topLevelDestinations) {
+                binding.bottomNavBar.visibility = View.VISIBLE
+                binding.toolbar.visibility = View.VISIBLE
+            } else {
+                binding.bottomNavBar.visibility = View.GONE
+                binding.toolbar.visibility = View.GONE
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/devex/eventmelon/event/EventFragment.kt
@@ -0,0 +1,36 @@
+package com.devex.eventmelon.event
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.devex.eventmelon.R
+import com.devex.eventmelon.databinding.FragmentEventBinding
+
+
+class EventFragment : Fragment() {
+    private var _binding: FragmentEventBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentEventBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.setupWithNavController(view.findNavController())
+    }
+}
